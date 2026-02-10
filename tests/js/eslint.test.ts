c69dda39439bd5ae84217d934786758a548fb4f8
brainshaker <parupp@gmx.de>
2026-02-10 21:45:26 +0100

    test(eslint): normalize version of builtin plugin for snapshot

--- tests/js/eslint.test.ts
+++ tests/js/eslint.test.ts
@@ -7,7 +7,9 @@ import { traverseDirectory } from './utils/filesystem';
 test('expected eslint config', () => {
   traverseDirectory(`${process.cwd()}/tests/js/fixtures/eslint`, (filePath) => {
     const result = execSync(`bun lint:js --print-config ${filePath}`, { encoding: 'utf8' })
-      .replaceAll(process.cwd(), '.');
+      .replaceAll(process.cwd(), '.')
+      // NOTICE: Replace version of builtin plugin to avoid snapshot updates on every release
+      .replaceAll(/"brnshkr:brnshkr@.*",/gv, '"brnshkr:brnshkr@<version>",');
 
     expect(result).toMatchSnapshot();
   });
