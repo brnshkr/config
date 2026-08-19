d8116a5c3311d5c6f94fb02c9bca6ef86d256e45
brainshaker <parupp@gmx.de>
2026-08-19 19:53:14 +0200

    fix(js/stylelint): keep `ignoreFiles` globs matching anywhere

--- src/js/stylelint/configs/ignores.ts
+++ src/js/stylelint/configs/ignores.ts
@@ -5,6 +5,6 @@ import type { Config } from '../types/config';
 // eslint-disable-next-line brnshkr/boolish-prefix -- Config builders are named after the config section they build
 export const ignores = (): Config[] => [
   {
-    ignoreFiles: GLOB_IGNORES.map((glob) => glob.replace(/^\*\*/v, process.cwd())),
+    ignoreFiles: GLOB_IGNORES.map((glob) => glob.replace(/^\*\*/v, `${process.cwd()}/**`)),
   },
 ];
