bea758968818cd48ce13ceabef3dd834f147eda3
brainshaker <parupp@gmx.de>
2026-09-14 20:57:11 +0200

    test(js): anchor typescript snapshot at project root

--- tests/js/typescript.test.ts
+++ tests/js/typescript.test.ts
@@ -3,5 +3,5 @@ import { expect, test } from 'vitest';
 import { run } from './utils/command';
 
 test('expected typescript config', () => {
-  expect(run('bun tsc --showConfig --project conf/tsconfig.json')).toMatchSnapshot();
+  expect(run('bun tsc --showConfig --project conf/tsconfig.json').replaceAll('./conf/', './')).toMatchSnapshot();
 });
